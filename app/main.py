07f653979e283ea3e2c232f52f68119fd3721597
Rao  Shahroz Ali <raoshahrozali@Devflovvs-MacBook-Pro.local>
2026-09-09 18:23:38 +0500

    Allow production frontend in CORS

--- app/main.py
+++ app/main.py
@@ -22,7 +22,9 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://stock-exchange-frontend-piek.onrender.com",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
